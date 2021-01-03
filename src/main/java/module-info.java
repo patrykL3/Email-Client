190785ebe57c6288cbe775d44824dc7d7c4d2776
Patryk Łubik <patryk.lubik@wp.pl>
2021-01-03 11:28:55 +0100

    skeleton

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module org.openjfx {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.web;
+    exports pl.patryklubik;
+}
---
+++ src/main/java/pl/patryklubik/Launcher.java
@@ -0,0 +1,20 @@
+package pl.patryklubik;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+/**
+ * JavaFX App
+ */
+public class Launcher extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
