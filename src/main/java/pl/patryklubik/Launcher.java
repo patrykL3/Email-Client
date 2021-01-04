a46cc1a19aa9504fd516069e6f11b18db8e772ce
Patryk Łubik <patryk.lubik@wp.pl>
2021-01-04 17:22:00 +0100

    login window

--- src/main/java/pl/patryklubik/Launcher.java
+++ src/main/java/pl/patryklubik/Launcher.java
@@ -1,6 +1,9 @@
 package pl.patryklubik;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
@@ -11,10 +14,15 @@ public class Launcher extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        Parent parent = FXMLLoader.load(getClass().getResource("LoginWindow.fxml"));
+
+        Scene scene = new Scene(parent, 510, 325);
+        stage.setScene(scene);
+
+        stage.show();
     }
 
     public static void main(String[] args) {
         launch();
     }
-
 }
---
+++ src/main/java/pl/patryklubik/view/LoginWindowController.java
@@ -0,0 +1,24 @@
+package pl.patryklubik.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginWindowController {
+
+    @FXML
+    private TextField emailAddressFied;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    void loginButtonAction() {
+
+    }
+
+}
