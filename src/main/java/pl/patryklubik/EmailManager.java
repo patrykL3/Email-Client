69a92353bbe9a3d6936e52a4de6bc71e32d5a0bb
Patryk Łubik <patryk.lubik@wp.pl>
2021-01-09 19:57:18 +0100

    updating folders

--- src/main/java/pl/patryklubik/EmailManager.java
+++ src/main/java/pl/patryklubik/EmailManager.java
@@ -2,8 +2,13 @@ package pl.patryklubik;
 
 import javafx.scene.control.TreeItem;
 import pl.patryklubik.controller.services.FetchFoldersService;
+import pl.patryklubik.controller.services.FolderUpdaterService;
 import pl.patryklubik.model.EmailAccount;
 import pl.patryklubik.model.EmailTreeItem;
+
+import javax.mail.Folder;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Create by Patryk Łubik on 04.01.2021.
@@ -13,9 +18,20 @@ public class EmailManager {
     //Folder handling:
     private TreeItem<String> foldersRoot = new TreeItem<String>("");
     private EmailTreeItem<String> selectedFolder;
+    private FolderUpdaterService folderUpdaterService;
+    private List<Folder> folderList = new ArrayList<Folder>();
 
     public TreeItem<String> getFoldersRoot(){
         return foldersRoot;
+    }
+
+    public  List<Folder> getFolderList(){
+        return this.folderList;
+    }
+
+    public EmailManager(){
+        folderUpdaterService = new FolderUpdaterService(folderList);
+        folderUpdaterService.start();
     }
 
     public void setSelectedFolder(EmailTreeItem<String> selectedFolder) {
@@ -24,7 +40,7 @@ public class EmailManager {
 
     public void addEmailAccount(EmailAccount emailAccount){
         EmailTreeItem<String> treeItem = new EmailTreeItem<String>(emailAccount.getAddress());
-        FetchFoldersService fetchFoldersService = new FetchFoldersService(emailAccount.getStore(), treeItem);
+        FetchFoldersService fetchFoldersService = new FetchFoldersService(emailAccount.getStore(), treeItem, folderList);
         fetchFoldersService.start();
         foldersRoot.getChildren().add(treeItem);
     }
